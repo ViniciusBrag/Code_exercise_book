d5968236765c6af5309e4309bebe7e677a130f61
Vinicius Braga <Vbragadev@gmail.com>
2022-06-23 00:54:14 -0300

    add requirements.txt because is utils some libs as: flake8 etc...

---
+++ Generate_files_random/randomQuizGenerator.py
@@ -0,0 +1,69 @@
+#!python3
+
+# randomQuizGenerator.py - Create tests with ask and answer
+
+import random
+
+CAPITALS = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+}
+
+quizNum = [quiz for quiz in range(35)]
+quizFile = open("capitalquiz%s.txt" % (quizNum + 1), "w")
+answerKeyFile = open("capitalquiz_answers%s.txt" % (quizNum), "w")
+
+quizFile.write("Name:\n\n Date:\n\nPeriod:\n\n")
+quizFile.write(("" * 20) + "State Capital Quiz (Form %s)" % (quizNum + 1))
+quizFile.write("\n\n")
+
+states = list(CAPITALS.keys())
+random.shuffle(states)
