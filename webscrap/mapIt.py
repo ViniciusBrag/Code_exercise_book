43a3078227f986c8b8d2b4e8233bdfaa424c6678
Vinicius Braga <Vbragadev@gmail.com>
2023-02-02 22:45:23 -0300

    add code that search address at Google Maps

---
+++ webscrap/mapIt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import webbrowser, sys
+import pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open(f'https://www.google.com/maps/place/{address}')
